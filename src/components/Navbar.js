51024943c0d34bbdf3fd7d65e0cc04c2059e0188
Mo <mo2005@hotmail.co.uk>
2022-02-20 16:53:54 +0000

    Background backup particlejs react

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import '../styles/Navbar.css'
 const Navbar = () => {
     return ( 
         <div className="nav-container">
-            <nav className="navbar">
+            <nav className="container navbar">
                 <a href="/">Home</a>
                 <a href="/">About</a>
                 <a href="/">Projects</a>
