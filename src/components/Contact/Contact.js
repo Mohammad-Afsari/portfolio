c07396e8b8ee1fa1b2a99273de345dd0cccfff2e
Mo <mo2005@hotmail.co.uk>
2022-02-27 21:38:53 +0000

    Added contact page rev01

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,57 @@
 import './Contact.css'
+import CV from '../Assets/Mohammad-A_CV.pdf'
+import aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+    useEffect(() => {
+        aos.init({duration: 3000});
+    }, [])
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cv0ctrp', 'template_q6i79m7', form.current, 'nvCHZpBsc5b4ao8bd')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+    e.target.reset();
+    };
+
     return ( 
-        <section className="contact">
-            
-        </section>
+        <div className="contact">
+            <section className="contactInfo" data-aos="fade-up">
+                <div className="locationMessage">
+                    <p>For more detailed information, please feel free to download my resume below.</p>
+                </div>
+                <div className="cv"><a href={CV} download>Download Resume</a></div>
+                <div className="locationMessage">
+                    <p>Based in:</p>
+                </div>
+                <div className="location">Farringdon, London, United Kingdom</div>
+            </section>
+            <section className="formContainer" data-aos="fade-up">
+                <div className="message">
+                    <p>For <span>freelance</span> projects, <span>job</span> opportunities or if you wish to just have a <span>general chat,</span> please feel free to get in touch by filling out the <span>form</span> below. You can also reach out to me on <span>LinkedIn</span>, <span>email</span> or by <span>phone</span> (details held in my resume).</p>
+                </div>
+                <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                    <input type="text" placeholder='Your Full Name' name='name' required></input>
+                    <input type="email" placeholder='Your Email' name='email' required></input>
+                    <input type="text" placeholder='Subject' name='subject' required></input>
+                    <textarea placeholder='Your Message' rows="7" name="message" required></textarea>
+                    <button type='submit' className='submitBtn'>Send</button>
+                </form>
+            </section>
+        </div>
      );
 }
- 
+// AIzaSyDl8_zl_A18LTX1mQbUFbf7Mn8KfZE82Mc
 export default Contact;
