861857df798d703b8243d0f7ca8ab833069d9ef1
Mo <mo2005@hotmail.co.uk>
2022-02-28 11:47:52 +0000

    final draft rev01

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,8 @@
 import './Footer.css'
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear()
+
     return ( 
         <div className="footer">
             <section className='contactHoursContainer'>
@@ -9,6 +11,7 @@ const Footer = () => {
                     <p><span>Mon-Fri:</span> 08:00 - 20:00</p>
                     <p><span>Sat:</span> 08:00 - 12:00</p>
                     <p><span>Sun:</span> 10:00 - 14:00</p>
+                    <p>Copyright &copy; {currentYear}  M.A All Rights Reserved</p>
                 </div>
             </section>
             <section className="footerDetails">
