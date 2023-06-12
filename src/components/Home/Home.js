cff4b8c08c4a19e77be033e479b6f75587171df8
Mohammad Afsari <mohammad.afsari.pro@gmail.com>
2023-06-12 23:15:37 +0100

    feat: update skills, add new project card component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,88 +1,88 @@
-import "./Home.css";
-import React, { useEffect, useState } from "react";
-import TextTransition, { presets } from "react-text-transition";
-import profilePicCroppedHD from "../imgs/profilePicCroppedHD-min.jpg";
-import { SiGithub } from "react-icons/si";
-import { SiLinkedin } from "react-icons/si";
-import { MdEmail } from "react-icons/md";
-import { FaFileDownload } from "react-icons/fa";
-import aos from "aos";
-import "aos/dist/aos.css";
-import CV from "../Assets/Mohammad-A_CV.pdf";
+import './Home.css'
+import React, { useEffect, useState } from 'react'
+import TextTransition, { presets } from 'react-text-transition'
+import profilePicCroppedHD from '../imgs/profilePicCroppedHD-min.jpg'
+import { SiGithub } from 'react-icons/si'
+import { SiLinkedin } from 'react-icons/si'
+import { MdEmail } from 'react-icons/md'
+import { FaFileDownload } from 'react-icons/fa'
+import aos from 'aos'
+import 'aos/dist/aos.css'
+import CV from '../Assets/Mohammad-A_CV.pdf'
 
-const messages = ["Frontend Developer", "Engineer", "Freelancer"];
+const messages = ['Frontend Developer', 'Engineer', 'Freelancer']
 
 const Home = () => {
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0)
 
-  useEffect(() => {
-    const intervalId = setInterval(
-      () => setIndex((index) => index + 1),
-      3000 // every 3 seconds
-    );
-    return () => clearTimeout(intervalId);
-  }, []);
+    useEffect(() => {
+        const intervalId = setInterval(
+            () => setIndex((index) => index + 1),
+            3000
+        )
+        return () => clearTimeout(intervalId)
+    }, [])
 
-  useEffect(() => {
-    aos.init({ duration: 3000 });
-  }, []);
+    useEffect(() => {
+        aos.init({ duration: 3000 })
+    }, [])
 
-  return (
-    <section className="container section" data-aos="fade-up" id="home">
-      <div className="intro">
-        <div className="welcome">
-          <h1>Hi,</h1>
-          <p>I'm Mohammad Afsari </p>
-          <h2>
-            <TextTransition
-              className="transition-text"
-              text={messages[index % messages.length]}
-              springConfig={presets.wobbly}
-              noOverflow={true}
-            />
-          </h2>
-          <section className="links">
-            <p>
-              <a href="#contact">
-                <MdEmail className="link-item" />
-              </a>
-            </p>
-            <p>
-              <a
-                href="https://github.com/Mohammad-Afsari"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <SiGithub className="link-item" />
-              </a>
-            </p>
-            <p>
-              <a
-                href="https://www.linkedin.com/in/mohammad-afsari-77432b7b/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <SiLinkedin className="link-item" />
-              </a>
-            </p>
-            <p>
-              <a
-                href={CV} 
-                download
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaFileDownload className="link-item" />
-              </a>
-            </p>
-          </section>
-        </div>
-      </div>
-      <div className="pictureContainer">
-        <img className="profilePic" src={profilePicCroppedHD} alt="" />
-      </div>
-    </section>
-  );
-};
+    return (
+        <section className="container section" data-aos="fade-up" id="home">
+            <div className="intro">
+                <div className="welcome">
+                    <h1>Hi,</h1>
+                    <p>I'm Mohammad Afsari </p>
+                    <h2>
+                        <TextTransition
+                            className="transition-text"
+                            text={messages[index % messages.length]}
+                            springConfig={presets.wobbly}
+                            noOverflow={true}
+                        />
+                    </h2>
+                    <section className="links">
+                        <p>
+                            <a href="#contact">
+                                <MdEmail className="link-item" />
+                            </a>
+                        </p>
+                        <p>
+                            <a
+                                href="https://github.com/Mohammad-Afsari"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <SiGithub className="link-item" />
+                            </a>
+                        </p>
+                        <p>
+                            <a
+                                href="https://www.linkedin.com/in/mohammad-afsari-77432b7b/"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <SiLinkedin className="link-item" />
+                            </a>
+                        </p>
+                        <p>
+                            <a
+                                href={CV}
+                                download
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <FaFileDownload className="link-item" />
+                            </a>
+                        </p>
+                    </section>
+                </div>
+            </div>
+            <div className="pictureContainer">
+                <img className="profilePic" src={profilePicCroppedHD} alt="" />
+            </div>
+        </section>
+    )
+}
 
-export default Home;
+export default Home
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,109 +1,109 @@
-import "./Skills.css";
-import { SiHtml5 } from "react-icons/si";
-import { SiCss3 } from "react-icons/si";
-import { SiJavascript } from "react-icons/si";
-import { SiBootstrap } from "react-icons/si";
-import { SiMaterialui } from "react-icons/si";
-import { SiReact } from "react-icons/si";
-import { SiTypescript } from "react-icons/si";
-import { DiMsqlServer } from "react-icons/di";
-import { SiPostgresql } from "react-icons/si";
-import { SiSupabase } from "react-icons/si";
-import { SiPython } from "react-icons/si";
-import { SiPowerbi } from "react-icons/si";
-import { SiGit } from "react-icons/si";
-import { SiJest } from "react-icons/si";
-import { SiPostcss } from "react-icons/si";
-import { SiAmazondynamodb } from "react-icons/si";
-import { GrGraphQl } from "react-icons/gr";
-import { SiStorybook } from "react-icons/si";
-import { SiTestinglibrary } from "react-icons/si";
+import './Skills.css'
+import { SiHtml5 } from 'react-icons/si'
+import { SiCss3 } from 'react-icons/si'
+import { SiJavascript } from 'react-icons/si'
+import { SiBootstrap } from 'react-icons/si'
+import { SiMaterialui } from 'react-icons/si'
+import { SiReact } from 'react-icons/si'
+import { SiTypescript } from 'react-icons/si'
+import { DiMsqlServer } from 'react-icons/di'
+import { SiPostgresql } from 'react-icons/si'
+import { SiSupabase } from 'react-icons/si'
+import { SiPython } from 'react-icons/si'
+import { SiPowerbi } from 'react-icons/si'
+import { SiGit } from 'react-icons/si'
+import { SiJest } from 'react-icons/si'
+import { SiPostcss } from 'react-icons/si'
+import { SiAmazondynamodb } from 'react-icons/si'
+import { GrGraphQl } from 'react-icons/gr'
+import { SiStorybook } from 'react-icons/si'
+import { SiTestinglibrary } from 'react-icons/si'
 
 const Skills = () => {
-  return (
-    <div className="skillsContainer">
-      <section className="front-end">
-        <h2>Frontend</h2>
-        <div className="front-end skillItem">
-          <SiHtml5 /> <span>HTML5</span>
+    return (
+        <div className="skillsContainer">
+            <section className="front-end">
+                <h2>Frontend</h2>
+                <div className="front-end skillItem html">
+                    <SiHtml5 /> <span>HTML5</span>
+                </div>
+                <div className="front-end skillItem css">
+                    <SiCss3 /> <span>CSS3</span>
+                </div>
+                <div className="front-end skillItem postcss">
+                    <SiPostcss /> <span>PostCSS</span>
+                </div>
+                <div className="front-end skillItem javascript">
+                    <SiJavascript /> <span>JavaScript</span>
+                </div>
+                <div className="front-end skillItem typescript">
+                    <SiTypescript /> <span>TypeScript</span>
+                </div>
+                <div className="front-end skillItem react">
+                    <SiReact /> <span>React (Hooks, zustand, react-query)</span>
+                </div>
+            </section>
+            <section className="back-end">
+                <h2>Backend (Database Systems)</h2>
+                <div className="back-end skillItem tsql">
+                    <DiMsqlServer />
+                    <span>SQL Server (T-SQL)</span>
+                </div>
+                <div className="back-end skillItem postgresql">
+                    <SiPostgresql />
+                    <span>PostgreSQL</span>
+                </div>
+                <div className="back-end skillItem dynamodb">
+                    <SiAmazondynamodb />
+                    <span>DynamoDB</span>
+                </div>
+                <div className="back-end skillItem supabase">
+                    <SiSupabase />
+                    <span>Supabase</span>
+                </div>
+            </section>
+            <section className="miscellaneous">
+                <h2>Miscellaneous</h2>
+                <div className="miscellaneous skillItem jest">
+                    <SiJest />
+                    <span>Jest</span>
+                </div>
+                <div className="miscellaneous skillItem reactTestingLibrary">
+                    <SiTestinglibrary />
+                    <span>React Testing Library</span>
+                </div>
+                <div className="tools skillItem storybook">
+                    <SiStorybook />
+                    <span>Storybook</span>
+                </div>
+                <div className="miscellaneous skillItem python">
+                    <SiPython />
+                    <span>Python</span>
+                </div>
+                <div className="miscellaneous skillItem powerbi">
+                    <SiPowerbi /> <span>PowerBI</span>
+                </div>
+            </section>
+            <section className="tools">
+                <h2>Tools</h2>
+                <div className="tools skillItem graphql">
+                    <GrGraphQl />
+                    <span>GraphQL</span>
+                </div>
+                <div className="tools skillItem git">
+                    <SiGit /> <span>Git</span>
+                </div>
+                <div className="tools skillItem mui">
+                    <SiMaterialui />
+                    <span>Material UI</span>
+                </div>
+                <div className="tools skillItem bootstrap">
+                    <SiBootstrap />
+                    <span>Bootstrap</span>
+                </div>
+            </section>
         </div>
-        <div className="front-end skillItem">
-          <SiCss3 /> <span>CSS3</span>
-        </div>
-        <div className="front-end skillItem">
-          <SiPostcss /> <span>PostCSS</span>
-        </div>
-        <div className="front-end skillItem">
-          <SiJavascript /> <span>JavaScript</span>
-        </div>
-        <div className="front-end skillItem">
-          <SiTypescript /> <span>TypeScript</span>
-        </div>
-        <div className="front-end skillItem">
-          <SiReact /> <span>React (Hooks, zustand, react-query)</span>
-        </div>
-      </section>
-      <section className="back-end">
-        <h2>Backend (Database Systems)</h2>
-        <div className="back-end skillItem">
-          <DiMsqlServer />
-          <span>SQL Server (T-SQL)</span>
-        </div>
-        <div className="back-end skillItem">
-          <SiPostgresql />
-          <span>PostgreSQL</span>
-        </div>
-        <div className="back-end skillItem">
-          <SiAmazondynamodb />
-          <span>DynamoDB</span>
-        </div>
-        <div className="back-end skillItem">
-          <SiSupabase />
-          <span>Supabase</span>
-        </div>
-      </section>
-      <section className="data-analysis">
-        <h2>Miscellaneous</h2>
-        <div className="data-analysis skillItem">
-          <SiJest />
-          <span>Jest</span>
-        </div>
-        <div className="data-analysis skillItem">
-          <SiTestinglibrary />
-          <span>React Testing Library</span>
-        </div>
-        <div className="tools skillItem">
-          <SiStorybook />
-          <span>Storybook</span>
-        </div>
-        <div className="data-analysis skillItem">
-          <SiPython />
-          <span>Python</span>
-        </div>
-        <div className="data-analysis skillItem">
-          <SiPowerbi /> <span>PowerBI</span>
-        </div>
-      </section>
-      <section className="tools">
-        <h2>Tools</h2>
-        <div className="tools skillItem">
-          <GrGraphQl /> 
-          <span>GraphQL</span>
-        </div>
-        <div className="tools skillItem">
-          <SiGit /> <span>Git</span>
-        </div>
-        <div className="tools skillItem">
-          <SiMaterialui /> 
-          <span>Material UI</span>
-        </div>
-        <div className="tools skillItem">
-          <SiBootstrap /> 
-          <span>Bootstrap</span>
-        </div>
-      </section>
-    </div>
-  );
-};
+    )
+}
 
-export default Skills;
+export default Skills
